891db8a2956188c7590296549ca0c935b4be744f
Knut Melvær <knut.melvaer@gmail.com>
2018-12-09 15:06:22 +0100

    move to own module

--- src/convertHTMLtoPortableText.js
+++ src/convertHTMLtoPortableText.js
@@ -1,6 +1,40 @@
+const blockTools = require('@sanity/block-tools').default
+const jsdom = require('jsdom')
+const {JSDOM} = jsdom
 
-async function convertHTMLtoPortableText(html) {
-  return html
+const defaultSchema = require('./defaultSchema')
+
+const blockContentType = defaultSchema.get('blogPost').fields.find(field => field.name === 'body')
+  .type
+
+
+async function convertHTMLtoPortableText(HTMLDoc) {
+  const rules = [
+    {
+      // Special case for code blocks (wrapped in pre and code tag)
+      deserialize(el, next, block) {
+        if (el.tagName.toLowerCase() !== 'pre') {
+          return undefined
+        }
+        const code = el.children[0]
+        const childNodes =
+          code && code.tagName.toLowerCase() === 'code' ? code.childNodes : el.childNodes
+        let text = ''
+        childNodes.forEach(node => {
+          text += node.textContent
+        })
+        return block({
+          _type: 'code',
+          text: text
+        })
+      }
+    }
+  ]
+
+  const portableText = blockTools.htmlToBlocks(HTMLDoc, blockContentType, {
+    rules,
+    parseHtml: html => new JSDOM(html).window.document
+  })
 }
 
 module.exports = convertHTMLtoPortableText
--- src/convertToSanityDocument.js
+++ src/convertToSanityDocument.js
@@ -1,47 +1,7 @@
-const blockTools = require('@sanity/block-tools').default
-const convertToPortableText = require('./convertHTMLtoPortableText')
-const jsdom = require('jsdom')
-const {JSDOM} = jsdom
-
-const defaultSchema = require('./defaultSchema')
-
-const blockContentType = defaultSchema.get('blogPost').fields.find(field => field.name === 'body')
-  .type
-
-function convertToSanityDocument({data, contents}) {
+const convertHTMLtoPortableText = require('./convertHTMLtoPortableText')
+async function convertToSanityDocument({data, contents}) {
   const { title, date, spoiler } = data.frontmatter
-  const HTMLdocument = `<html>
-  <body>
-    <em>Hei</em>
-    <pre><code>const foo = 'bar'</code></pre>
-  </body>
-</html>`
-
-  const rules = [
-    {
-      // Special case for code blocks (wrapped in pre and code tag)
-      deserialize(el, next, block) {
-        if (el.tagName.toLowerCase() !== 'pre') {
-          return undefined
-        }
-        const code = el.children[0]
-        const childNodes =
-          code && code.tagName.toLowerCase() === 'code' ? code.childNodes : el.childNodes
-        let text = ''
-        childNodes.forEach(node => {
-          text += node.textContent
-        })
-        return block({
-          _type: 'code',
-          text: text
-        })
-      }
-    }
-  ]
-
-  const portableText = blockTools.htmlToBlocks(HTMLdocument, blockContentType, {rules})
-
-
+  const portableText = await convertHTMLtoPortableText(contents)
 
   const doc = {
     _type: 'post',
